7780f0618f4a5af85c39b88e234e35bc90010f03
Crisano97 <97keb6@gmail.com>
2022-08-24 17:31:43 +0200

    subquestion management with a foreach

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $contents = [
         [
             'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
-            'subquestion' => '',
+            'subquestions' => [],
             'answer' => [
                 'La recente <a href="#">decisione della Corte di giustizia dell\'Unione europea </a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.',
                 "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
@@ -13,7 +13,7 @@
         ],
         [
             'question' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
-            'subquestion' => '',
+            'subquestions' => [],
             'answer' => [
                 "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
                 "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
@@ -23,7 +23,7 @@
         ],
         [
             'question' => 'Perché il mio account è associato a un paese?',
-            'subquestion' => '',
+            'subquestions' => [],
             'answer' => [
                 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:',
                 '<ol>
@@ -41,7 +41,7 @@
         ],
         [
             'question' => '',
-            'subquestion' => 'Stabilire il paese associato al tuo account',
+            'subquestions' => ['Stabilire il paese associato al tuo account'],
             'answer' => [
                 "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
                 "I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.",
@@ -50,14 +50,14 @@
         ],
         [
             'question' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
-            'subquestion' => '',
+            'subquestions' => [],
             'answer' => [
             'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href="#">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href="#"> visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a>',
             ]
         ],
         [
             'question' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
-            'subquestion' => '',
+            'subquestions' => [],
             'answer' => [
                 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\' esatte parole chiave che hanno determinato il clic su un annuncio.',
             ]
@@ -89,9 +89,17 @@
                         <?php  echo $content['question']; ?>
                     </h2>
 
-                    <h4>
-                        <?php echo $content['subquestion'] ?>
-                    </h4>
+                    
+                    <?php foreach($content['subquestions'] as $subquestion){ ?>
+                        <h4>
+                            <?php 
+                                echo $subquestion;
+                            ?>
+                        </h4>
+                    <?php } ?>  
+                    
+
+                    
                     <?php foreach($content['answer'] as $answer){ ?>
                         <p>
                             <?php echo $answer; ?>
