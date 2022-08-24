64667e27736e615890dc6a374430b31127b260d7
Crisano97 <97keb6@gmail.com>
2022-08-24 16:24:15 +0200

    nesting an ordered list in another ordered list

--- index.php
+++ index.php
@@ -18,7 +18,36 @@
                 'Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href="#">Centro Google per la sicurezza online.</a>',
                 "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
             ]
-        ]
+        ],
+        [
+            'question' => 'Perché il mio account è associato a un paese?',
+            'answer' => [
+                'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:',
+                '<ol>
+                    <li>
+                        La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                        <ol class="inner-list">
+                            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                            <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                        </ol>
+                    </li>
+                    <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+                </ol>',
+                'Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.',
+            ]
+        ],
+        [
+            'question' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+            'answer' => [
+                
+            ]
+        ],
+        [
+            'question' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+            'answer' => [
+                
+            ]
+        ],
     ]
 ?>
 
@@ -33,16 +62,6 @@
 </head>
 <body>
     <div class="container">
-        <?php 
-            // foreach($contents as $content){
-            //     // var_dump($content);
-            //     echo $content['question'];
-            //     foreach($content['answer'] as $answer){
-            //         echo $answer;
-            //     }
-            // }
-        ?>
-
         
             <?php foreach($contents as $content){ ?>
                 <h2> 
